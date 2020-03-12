c2a738234cea7baa84e7e772ced6e8d5d5ab8d7a
sayhoChun <fishcreek@naver.com>
2020-03-12 15:46:03 +0900

    init

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import App from "../src/App"
+
+Vue.use(VueRouter)
+
+export const router = new VueRouter({
+    routes: [
+        {
+            path: '/',
+            component: App
+        },
+        {
+            path: '/news',
+            component: App
+        }
+    ]
+})
