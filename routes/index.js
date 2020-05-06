548475a9c015b47e4279f3485075e22fb326a287
sayhoChun <fishcreek@naver.com>
2020-05-06 13:35:40 +0900

    lazy load added

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import Home from "../src/components/Home";
-import TableView from "../src/components/TableView";
-import SidoTable from "../src/components/SidoTable";
-import UserList from "../src/components/UserList";
-import UserDetail from "../src/components/UserDetail";
-import ChatList from "../src/components/ChatList";
-import Chat from "../src/components/Chat";
+// import Home from "../src/components/Home";
+// import TableView from "../src/components/TableView";
+// import SidoTable from "../src/components/SidoTable";
+// import UserList from "../src/components/UserList";
+// import UserDetail from "../src/components/UserDetail";
+// import ChatList from "../src/components/ChatList";
+// import Chat from "../src/components/Chat";
+
+function loadView(view) {
+    return () => import(/* webpackChunkName: "view-[request]" */ `../src/components/${view}.vue`)
+}
 
 Vue.use(VueRouter);
 
@@ -16,18 +20,18 @@ export const router = new VueRouter({
     base: __dirname,
     routes: [
         {path: "/", redirect: "/home"},
-        {path: "/home", component: Home},
-        {path: '/region', component: TableView},
-        {path: "/region/:id", name: "sido", component: SidoTable},
+        {path: "/home", component: loadView("Home")},
+        {path: '/region', component: loadView("TableView")},
+        {path: "/region/:id", name: "sido", component: loadView("SidoTable")},
         {
             path: "/userList",
             name: "userList",
-            component: UserList,
+            component: loadView("UserList"),
             children: [
-                {path: "detail/:id", name: "userDetail", component: UserDetail}
+                {path: "detail/:id", name: "userDetail", component: loadView("UserDetail")}
             ]
         },
-        {path: "/chatList", name: "chatList", component: ChatList},
-        {path: "/chatList/:id", name: "chat", component: Chat}
+        {path: "/chatList", name: "chatList", component: loadView("ChatList")},
+        {path: "/chatList/:id", name: "chat", component: loadView("Chat")}
     ]
 });
