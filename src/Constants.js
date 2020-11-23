ba71cf77ba7073821c892b57262983d3c5c82515
sayhoChun <fishcreek@naver.com>
2020-11-24 00:55:22 +0900

    merged & version migration

--- src/Constants.js
+++ src/Constants.js
@@ -1,6 +1,8 @@
+const URL = "sayho.site";
+// const URL = "localhost";
+
 export default Object.freeze({
-    // API_URL: "http://localhost:10050",
-    // WEB_SOCKET_URL: "ws://localhost:10060/socket",
-    API_URL: "http://sayho.site:10050",
-    WEB_SOCKET_URL: "ws://sayho.site:10060/socket",
+    API_URL: `http://${URL}:10050`,
+    WEB_SOCKET_URL: `ws://${URL}:10060/socket`,
+    IMG_URL: `http://${URL}:10080`
 })
